e00444a5cfcab392b253e35e0392fc60749ccc49
infinity0106 <berny.orozco@hotmail.com>
2020-07-25 09:54:31 -0700

    change electron versions

--- bin/cli.js
+++ bin/cli.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
 
-var path      = require('path');
-var join      = path.join;
-var fs        = require('fs');
-var program   = require('commander');
-var spawn     = require('child_process').spawn;
-var log       = console.log;
-var _         = require('lodash');
+var path = require('path');
+var join = path.join;
+var fs = require('fs');
+var program = require('commander');
+var spawn = require('child_process').spawn;
+var log = console.log;
+var _ = require('lodash');
 
 program
   .usage('[command] [options]')
   .version(require('../package.json').version)
-  .on('--version', function(){
+  .on('--version', function () {
     return require('../package.json').version;
   })
-  .on('--help', function(){
+  .on('--help', function () {
     log('  Examples:\n');
     log('    ' + [
       '# cd into meteor dir first',
@@ -40,7 +40,7 @@ program
   .option('-t, --temp     <path>', 'electrify temp folder | default = system temp folder')
   .option('-a, --arch     <arch>', 'arch to build for     | default = current arch')
   .option('-p, --platform <platform>', 'platform to build for | default = current platform')
-;
+  ;
 
 program
   .command('run')
@@ -62,48 +62,50 @@ program.parse(process.argv);
 
 // default command = run
 var cmd = process.argv[2];
-if(process.argv.length == 2 || -1 == 'run|bundle|package'.indexOf(cmd) ){
+if (process.argv.length == 2 || -1 == 'run|bundle|package'.indexOf(cmd)) {
   run();
 }
 
-function run_electron(){
-  var input         = program.input || process.cwd();
+function run_electron() {
+  var input = program.input || process.cwd();
   var electrify_dir = join(input, '.electrify');
   var electron_path = require('electron');
-  var settings      = parse_meteor_settings(true);
+  var settings = parse_meteor_settings(true);
 
-  if(settings)
+  if (settings)
     settings = {
       ELECTRIFY_SETTINGS_FILE: path.resolve(settings)
     };
   else
     settings = {};
 
-  log('[[[ electron ' + electrify_dir +'` ]]]');
+  _.extend(settings, process.env)
+
+  log('[[[ electron ' + electrify_dir + '` ]]]');
   spawn(electron_path, [electrify_dir], {
     stdio: 'inherit',
     env: _.extend(settings, process.env)
   });
 }
 
-function is_meteor_app(){
+function is_meteor_app() {
   var input = program.input || process.cwd();
   var meteor_dir = join(input, '.meteor');
   return fs.existsSync(meteor_dir);
 }
 
-function run(){
-  if(has_local_electrify())
+function run() {
+  if (has_local_electrify())
     run_electron();
-  else if(is_meteor_app())
+  else if (is_meteor_app())
     electrify().app.init(run_electron);
 }
 
-function bundle(){
+function bundle() {
   electrify().app.bundle(/* server_url */);
 }
 
-function package(){
+function package() {
   electrify().app.package(parse_packager_options());
 }
 
@@ -113,26 +115,29 @@ function electrify() {
   var input;
 
   // validates input dir (app-root folder)
-  if(program.input && !fs.existsSync(program.input)) {
+  if (program.input && !fs.existsSync(program.input)) {
     console.error('input folder doesn\'t exist\n  ' + program.input);
     process.exit();
   }
-  
+
   input = program.input || process.cwd();
 
-  if(!is_meteor_app()) {
+  if (!is_meteor_app()) {
     console.error('not a meteor app\n  ' + input);
     process.exit();
   }
 
-  if(program.output && !fs.existsSync(program.output)) {
-    console.error('output folder doesn\'t exist\n  ' + program.output);
-    process.exit();
+  program.output = program.output || '.electrify/.dist'
+  if (program.output && !fs.existsSync(program.output)) {
+    console.log('output folder doesn\'t exist\n  ' + program.output);
+    console.log('  creating one');
+    fs.mkdirSync(program.output);
   }
 
-  if(program.temp && !fs.existsSync(program.temp)) {
-    console.error('temp folder doesn\'t exist\n  ' + program.temp);
-    process.exit();
+  if (program.temp && !fs.existsSync(program.temp)) {
+    console.log('temp folder doesn\'t exist\n  ' + program.temp);
+    console.log('  creating one');
+    fs.mkdirSync(program.temp);
   }
 
   var buildSettings = _.pick(program, ['temp', 'arch', 'platform']);
@@ -148,13 +153,13 @@ function electrify() {
 
 
 
-function has_local_electrify(){
+function has_local_electrify() {
   // validates input dir (app-root folder)
-  if(program.input && !fs.existsSync(program.input)) {
+  if (program.input && !fs.existsSync(program.input)) {
     console.error('input folder doesn\'t exist\n  ' + program.input);
     process.exit();
   }
-  
+
   var input = program.input || process.cwd();
 
   // validates meteor project
@@ -164,26 +169,26 @@ function has_local_electrify(){
 
 
 function parse_meteor_settings(return_path_only) {
-  if(!program.settings)
+  if (!program.settings)
     return (return_path_only ? null : {});
 
   var relative = join(process.cwd(), program.settings);
   var absolute = path.resolve(program.settings);
   var settings = (absolute == program.settings ? absolute : relative);
 
-  if(!fs.existsSync(settings)) {
+  if (!fs.existsSync(settings)) {
     log('settings file not found: ', relative);
     process.exit();
   }
 
-  if(return_path_only)
+  if (return_path_only)
     return settings;
   else
     return require(settings);
 }
 
 
-function parse_packager_options(){
+function parse_packager_options() {
   var names = [
     '--icon',
     '--app-bundle-id',
@@ -203,19 +208,19 @@ function parse_packager_options(){
   var dashdash = process.argv.indexOf('--');
   var options = {};
 
-  if(~dashdash) {
+  if (~dashdash) {
 
-    var args = process.argv.slice(dashdash+1);
+    var args = process.argv.slice(dashdash + 1);
 
-    _.each(args, function(arg){
+    _.each(args, function (arg) {
 
       var parts = arg.split('=');
       var key = parts[0];
-      var val = 'undefined' === typeof(parts[1]) ? true : parts[1];
-    
-      if(~names.indexOf(key)) {
+      var val = 'undefined' === typeof (parts[1]) ? true : parts[1];
+
+      if (~names.indexOf(key)) {
         if (key === '--app-version') {
-            options.appVersion = val;
+          options.appVersion = val;
         } else {
           options[key.slice(2)] = val;
         }
